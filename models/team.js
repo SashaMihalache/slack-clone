a2a37fd08be8068f897a11a9958142fd7536fd72
sasha <sasha.mihalache@gmail.com>
2018-06-10 08:20:33 +0300

    Protected routes with graphql and react-router

--- models/team.js
+++ models/team.js
@@ -1,4 +1,3 @@
-
 export default (sequelize, DataTypes) => {
   const Team = sequelize.define('team', {
     name: {
--- slack-clone-client/src/routes/index.js
+++ slack-clone-client/src/routes/index.js
@@ -2,21 +2,48 @@ import React from 'react';
 import {
   BrowserRouter,
   Route,
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom';
+import decode from 'jwt-decode';
 
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
 import CreateTeam from './CreateTeam';
 
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    decode(token);
+    decode(refreshToken);
+  } catch (err) {
+    return false;
+  }
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/login' }} />
+        )
+    )}
+  />
+)
+
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route path='/' exact component={Home} />
+      <PrivateRoute path='/' exact component={Home} />
       <Route path='/register' exact component={Register} />
       <Route path='/login' exact component={Login} />
-      <Route path='/team/new' exact component={CreateTeam} />
+      <PrivateRoute path='/team/new' exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
 )

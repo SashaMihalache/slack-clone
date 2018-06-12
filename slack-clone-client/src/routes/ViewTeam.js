c079a5ada36bebd80e640b1320a34df096e54de7
sasha <sasha.mihalache@gmail.com>
2018-06-12 11:26:01 +0300

    psql transactions and bugfixing

--- slack-clone-client/src/routes/ViewTeam.js
+++ slack-clone-client/src/routes/ViewTeam.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import findIndex from 'lodash/findIndex';
-
+import { Redirect } from 'react-router-dom';
 import { allTeamsQuery } from '../graphql/team';
 import AppLayout from '../components/AppLayout';
 import Header from '../components/Header';
@@ -18,10 +18,16 @@ const ViewTeam = ({ data: { loading, error, allTeams }, match: { params: { teamI
     console.log(error);
   }
 
-  const teamIdx = teamId ? findIndex(allTeams, ['id', parseInt(teamId, 10)]) : 0;
+  if (!allTeams.length) {
+    return <Redirect to='/team/new' />;
+  }
+
+  const teamIdInteger = parseInt(teamId, 10);
+  const teamIdx = teamIdInteger ? findIndex(allTeams, ['id', teamIdInteger]) : 0;
   const team = allTeams[teamIdx];
 
-  const channelIdx = channelId ? findIndex(team.channels, ['id', parseInt(channelId, 10)]) : 0;
+  const channelIdInteger = parseInt(channelId, 10);
+  const channelIdx = channelIdInteger ? findIndex(team.channels, ['id', channelIdInteger]) : 0;
   const channel = team.channels[channelIdx];
 
   return (
@@ -32,16 +38,22 @@ const ViewTeam = ({ data: { loading, error, allTeams }, match: { params: { teamI
           letter: t.name.charAt(0).toUpperCase()
         }))}
         team={team} />
-      <Header channelName={channel.name} />
-      <Messages channelId={channel.id} >
-        <ul className="message-list">
-          <li />
-          <li />
-        </ul>
-      </Messages>
-      <SendMessage channelName={channel.name} >
-        <input type="text" placeholder="CSS Grid Layout Module" />
-      </SendMessage>
+      {
+        channel && (
+          <React.Fragment>
+            <Header channelName={channel.name} />
+            <Messages channelId={channel.id} >
+              <ul className="message-list">
+                <li />
+                <li />
+              </ul>
+            </Messages>
+            <SendMessage channelName={channel.name} >
+              <input type="text" placeholder="CSS Grid Layout Module" />
+            </SendMessage>
+          </React.Fragment>
+        )
+      }
     </AppLayout>
   )
 }
